f56c019adf2620ce746088e7e9a4bde09ff6c66c
Waseem AL Tamer <107763849+WaseemALTamer@users.noreply.github.com>
2026-05-13 04:41:47 +0100

    Delete RGBLightingPY directory

--- RGBLightingPY/TickSystem/Transitions/__init__.py
+++
@@ -1,6 +0,0 @@
-
-# the Transitions will contain there own TickController so they can be 
-# reusable in a different project when you need them
-
-from .TickController import TickController
-from .Uniform import Uniform
--- RGBLightingPY/TickSystem/__init__.py
+++
@@ -1,1 +0,0 @@
-from .TickController import TickController
